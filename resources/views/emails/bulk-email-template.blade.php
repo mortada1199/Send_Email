7fb916abb7546d955b8f09671bce5c3e466c5205
mortada1199 <murtada199815@gmail.com>
2024-12-29 10:51:17 +0200

    add resouce

---
+++ resources/views/emails/bulk-email-template.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Email Notification</title>
+</head>
+<body>
+    <h1>Hello, {{ $data['email'] }}</h1>
+    <p>{{ $data['customer_number'] }}</p>
+</body>
+</html>
